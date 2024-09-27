d0048688aeacc1cf5fb2e105bb3a7e24a11815ff
Paul <paul.lennon@atu.ie>
2024-09-27 16:11:36 +0100

    Creating project

---
+++ src/main/java/ie/atu/week3/Calc.java
@@ -0,0 +1,12 @@
+package ie.atu.week3;
+
+public class Calc {
+    public static void main(String[] args) {
+        //Setup Github
+        //Link to my local machine
+        //Create a branch
+        //Write add method
+        //Commit, push, pull request, update local
+        //repeat for all the other methods
+    }
+}
